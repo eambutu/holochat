4146aa4faa531e53c3f4107ec401537cf11b48be
phillip <kip.phil@gmail.com>
2019-01-10 01:57:34 -0500

    some changes

--- client.py
+++ client.py
@@ -1,21 +1,23 @@
 from PIL import Image
 import socket
 import sys
+import time
 
-BYTES_IN_IMAGE = 1228800
+BYTES_IN_IMAGE = 12288000
 
 # Create TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('192.168.2.249', 6667)
+server_address = ('192.168.2.249', 7000)
 print('Connecting to %s port %s' % server_address)
 sock.connect(server_address)
 
 try:
     #while True:
-    im = Image.open('./cyrus_resized.jpg')
+    im = Image.open('./cyrus_real.png')
     im_bytes = im.tobytes()
+    print(time.time())
     
     sock.sendall(im_bytes)
 
--- server.py
+++ server.py
@@ -2,20 +2,33 @@ from PIL import Image
 import socket
 import sys
 import struct
+import time
+import pygame
 
 BYTES_IN_IMAGE = 12288000
+#BYTES_IN_IMAGE = 16384000
+#BYTES_IN_IMAGE = 32768000
 
 # Create TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind socket to a port
 # server_address = ('localhost', 6666)
-server_address = ('192.168.2.249', 6667)
+server_address = ('192.168.2.249', 7000)
 print('Starting up on %s port %s' % server_address)
 sock.bind(server_address)
 
 # Listen for incoming connections
 sock.listen(1)
+
+pygame.init()
+w = 2560
+h = 1600
+size=(w,h)
+screen = pygame.display.set_mode(size)
+c = pygame.time.Clock()
+
+cur_time = time.time()
 
 def recv_exact(conn, length):
     buffer = b''
@@ -24,9 +37,19 @@ def recv_exact(conn, length):
         if not data:
             return data
         buffer += data
-        if (len(data) > 0):
-            print(len(buffer))
+        #if (len(data) > 0):
+        #    print(len(buffer))
     return buffer
+
+def convert_to_16_bit(byte_array):
+    converted_array = []
+    for idx in range(len(byte_array) // 2):
+        if not byte_array[2*idx] == 0 and not byte_array[2*idx + 1] == 0:
+            print("%d %d", byte_array[2*idx], byte_array[2*idx + 1])
+        converted_array.append(byte_array[2*idx] << 4 + byte_array[2*(idx)+1])
+    return converted_array
+
+import pdb; pdb.set_trace()
 
 while True:
     # Wait for connection
@@ -40,9 +63,17 @@ while True:
         while True:
             image_bytes = recv_exact(connection, BYTES_IN_IMAGE)
             if (len(image_bytes) > 0):
-                print(len(image_bytes))
-                image = Image.frombytes('RGB', (2560, 1600), image_bytes, 'raw')
-                image.show()
+                #print(len(image_bytes))
+                img = pygame.image.fromstring(image_bytes, size, 'RGB')
+                screen.blit(img,(0,0))
+                pygame.display.update()
+                pygame.event.poll()
+                #image = Image.frombytes('RGB', (2560, 1600), image_bytes, 'raw')
+                # print(time.time())
+                #image.show()
+                now_time = time.time()
+                print('from last time took %d ms' % (now_time - cur_time))
+                cur_time = now_time
                     
     finally:
         # Clean up connection
