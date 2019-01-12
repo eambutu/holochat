2a7cdd5861fe36eca4bede9d0db6271b6bcd8f70
Cyrus Tabrizi <ctabrizi@CyBook.fios-router.home>
2019-01-11 19:35:03 -0500

    add extra thread for reading

--- server.py
+++ server.py
@@ -4,78 +4,121 @@ import sys
 import struct
 import time
 import pygame
+from threading import Thread, Lock
+import queue
 
-BYTES_IN_IMAGE = 12288000
-#BYTES_IN_IMAGE = 16384000
-#BYTES_IN_IMAGE = 32768000
+#w = 500
+#h = 200
+w = 1200
+h = 800
+BYTES_IN_IMAGE = w * h * 3
 
-# Create TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class ListenThread(Thread):
+    def __init__(self, obj, lock):
+        Thread.__init__(self)
+        self._obj = obj
+        self._lock = lock
+        self._terminating = False
 
-# Bind socket to a port
-# server_address = ('localhost', 6666)
-server_address = ('192.168.2.249', 7000)
-print('Starting up on %s port %s' % server_address)
-sock.bind(server_address)
-
-# Listen for incoming connections
-sock.listen(1)
-
-pygame.init()
-w = 2560
-h = 1600
-size=(w,h)
-screen = pygame.display.set_mode(size)
-c = pygame.time.Clock()
-
-cur_time = time.time()
-
-def recv_exact(conn, length):
-    buffer = b''
-    while len(buffer) < length:
-        data = conn.recv(length - len(buffer))
-        if not data:
-            return data
-        buffer += data
-        #if (len(data) > 0):
-        #    print(len(buffer))
-    return buffer
-
-def convert_to_16_bit(byte_array):
-    converted_array = []
-    for idx in range(len(byte_array) // 2):
-        if not byte_array[2*idx] == 0 and not byte_array[2*idx + 1] == 0:
-            print("%d %d", byte_array[2*idx], byte_array[2*idx + 1])
-        converted_array.append(byte_array[2*idx] << 4 + byte_array[2*(idx)+1])
-    return converted_array
-
-import pdb; pdb.set_trace()
-
-while True:
-    # Wait for connection
-    print('Waiting for connection')
-    connection, client_address = sock.accept()
-
-    try:
-        print('Connection from ', client_address)
+    def destroy(self):
+        self._terminating = True
         
+    def recv_exact(self, conn, length):
+        buffer = b''
+        while len(buffer) < length:
+            data = conn.recv(length - len(buffer))
+            if not data:
+                return data
+            buffer += data
+            #if (len(data) > 0):
+            #    print(len(buffer))
+        return buffer
+
+    def run(self):
+        # Create TCP/IP socket
+        # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+
+        # Bind socket to a port
+        #server_address = ('localhost', 6666)
+        server_address = ('192.168.1.67', 6000)
+        #server_address = ('a0:ce:c8:0f:46:63', 6000)
+        print('Starting up on %s port %s' % server_address)
+        sock.bind(server_address)
+
+        # Listen for incoming connections
+        sock.listen(1)
+        
+        # Wait for connection
+        print('Waiting for connection')
+        connection, client_address = sock.accept()
+        print('Connection from ', client_address)
+        self._obj.connection_received()
+        while True:
+            try:
+                image_bytes = self.recv_exact(connection, BYTES_IN_IMAGE)
+                self._obj.post_new_data(image_bytes)
+            finally:
+                # Clean up connection
+                print('Clean up connection')
+                #connection.close()
+
+
+def process_byte_string(byte_string):
+    temp = bytearray(byte_string) 
+    for idx in range(len(temp) // 3):
+        temp[3*idx: 3*(idx + 1)] = byte_string[3*idx: 3*(idx + 1)][::-1]
+    print('reversed image bytes')
+    return bytes(temp)
+
+class Runner(object):
+    def __init__(self):
+        self._lock = Lock()
+        self._listener = ListenThread(self, self._lock)
+        self._listener.start()
+        self.image_queue = queue.Queue()
+        self.has_connection = False
+
+    def post_new_data(self, data):
+        self.image_queue.put(data)
+
+    def connection_received(self):
+        self.has_connection = True
+
+    def run(self):
+        while not self.has_connection:
+            time.sleep(0.01)
+        pygame.init()
+        size=(w,h)
+        screen = pygame.display.set_mode(size)
+        c = pygame.time.Clock()
+        image_count = 1
+
+        cur_time = time.time()
+        first_time = True
+       
         # Receieve the data in small chunks and retransmit it
         while True:
-            image_bytes = recv_exact(connection, BYTES_IN_IMAGE)
+            while self.image_queue.empty():
+                time.sleep(0.01)
+            image_bytes = self.image_queue.get()
             if (len(image_bytes) > 0):
-                #print(len(image_bytes))
+                print(len(image_bytes))
+                print("Took %f seconds to receive" % (time.time() - cur_time))
+                if first_time:
+                    pygame.display.set_mode((0, 0), pygame.NOFRAME)
+                    first_time = False
                 img = pygame.image.fromstring(image_bytes, size, 'RGB')
+                img = pygame.transform.flip(img, False, True)
                 screen.blit(img,(0,0))
                 pygame.display.update()
                 pygame.event.poll()
-                #image = Image.frombytes('RGB', (2560, 1600), image_bytes, 'raw')
-                # print(time.time())
-                #image.show()
                 now_time = time.time()
-                print('from last time took %d ms' % (now_time - cur_time))
+                print('from last time took %f seconds' % (now_time - cur_time))
                 cur_time = now_time
-                    
-    finally:
-        # Clean up connection
-        print('Clean up connection')
-        connection.close()
+                        
+        
+if __name__=='__main__':
+    runner = Runner()
+    runner.run()
